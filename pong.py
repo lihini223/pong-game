3ae94e39f6c42d987b4aad952ae04b388928a0ff
lihini223 <lihininisansala95@gmail.com>
2021-06-27 18:27:00 +0530

    add scoring

--- pong.py
+++ pong.py
@@ -127,10 +127,10 @@ while True:
                   font=("Courier", 24, "normal"))
 
     # # Paddle and ball collisions
-    # if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 50 and ball.ycor() > paddle_b.ycor() - 40):
-    #     ball.setx(340)
-    #     ball.dx *= -1
+    if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 50 and ball.ycor() > paddle_b.ycor() - 40):
+        ball.setx(340)
+        ball.dx *= -1
 
-    # if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_b.ycor() + 50 and ball.ycor() > paddle_b.ycor() - 40):
-    #     ball.setx(-340)
-    #     ball.dx *= -1
+    if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_b.ycor() + 50 and ball.ycor() > paddle_b.ycor() - 40):
+        ball.setx(-340)
+        ball.dx *= -1
